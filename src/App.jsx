00b64c963fbab532ab8c2db7ce2084c779dc1fc3
Michelijustin <michelijustin13@gmail.com>
2025-07-13 13:51:25 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Login from './pages/Login/Login';
 import Cadastro from './pages/Cadastro/Cadastro';
 import Cadastro_vagas from './pages/Cadastro/Cadastro_vagas';
 import Login_empresa from './pages/Login/login_empresa';
-import EmpresaVagas from './pages/EmpresaVagas/EmpresaVagas';
 
 
 export default function App() {
@@ -21,7 +20,6 @@ export default function App() {
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/cadastro_vagas" element={<Cadastro_vagas/>} />
         <Route path="/login_empresa" element={<Login_empresa/>} />
-        <Route path="/empresa/vagas" element={<EmpresaVagas />} />
       </Routes>
     </BrowserRouter>
   );
