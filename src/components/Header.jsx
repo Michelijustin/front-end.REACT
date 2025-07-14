d5ed7582f8f79fdf00b94949bc384168a4169d60
Gretzel <gretzelpenaloza@gmail.com>
2025-07-13 23:38:55 -0300

    add rota vagaEmpresa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,24 @@
 import { Link } from 'react-router-dom';
-import logo from '../assets/img/trabalho-gimp.png'
+import logo from '../assets/img/trabalho-gimp.png';
 
 export default function Header() {
   return (
-     <header>
+    <header>
       <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
-          <img src={logo} alt="Logo" width="80" height="70"></img>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+          <img src={logo} alt="Logo" width="80" height="70" />
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
           </button>
+
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
@@ -22,8 +31,10 @@ export default function Header() {
                 <Link className="nav-link" to="/empresa">Empresa</Link>
               </li>
             </ul>
+
             <form className="d-flex" role="search">
-              <Link to="/login" className="btn btn-dark">Entrar</Link>
+              <Link to="/login_empresa" className="btn btn-dark">Entrar</Link>
+              <Link to="/cadastro" className="btn btn-dark ms-2">Cadastre sua empresa</Link>
             </form>
           </div>
         </div>
--- src/pages/Login/Login_empresa.jsx
+++ src/pages/Login/Login_empresa.jsx
@@ -1,45 +1,69 @@
 import login from "../../assets/img/login7.png"
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './login.css'
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
-export default function Login_empresa() {
+export default function Login() {
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    // Aqui você pode validar os dados do login, por exemplo.
+    // Simulando login com sucesso:
+    const loginSucesso = true;
+
+    if (loginSucesso) {
+      navigate('/empresa_vagas'); // redireciona para a página de vagas
+    } else {
+      alert('Login inválido');
+    }
+  };
+
   return (
     <>
-    <Header />
-    <main className="main">
-          <div className="login">
-            <form className="col col-md-12">
-              <img src={login} className="img_login mx-auto d-block" height="170" width="170"/>
-              <h1 className="h3 mb-3 fw-normal text-center">Entrar</h1>
-              <div className="form-floating">
-                <input type="" className="form-control" id="floatingInput" placeholder="Empresa"/>
-                <label htmlFor="floatingInput">Empresa</label>
+      <Header />
+      <main className="main">
+        <div className="login">
+          <form className="col col-md-12" onSubmit={handleLogin}>
+            <img src={login} className="img_login mx-auto d-block" height="170" width="170"/>
+            <h1 className="h3 mb-3 fw-normal text-center">Entrar</h1>
+
+            <div className="form-floating">
+              <input type="text" className="form-control" id="empresa" placeholder="Empresa" />
+              <label htmlFor="empresa">Empresa</label>
+            </div>
+
+            <div className="form-floating">
+              <input type="email" className="form-control" id="email" placeholder="your-email@gmail.com" />
+              <label htmlFor="email">E-mail</label>
+            </div>
+
+            <div className="form-floating">
+              <input type="password" className="form-control" id="senha" placeholder="Senha" />
+              <label htmlFor="senha">Senha</label>
+            </div>
+
+            <div className="esqueceu_lembrar">
+              <div className="form-check">
+                <input type="checkbox" className="form-check-input" id="flexCheckDefault" />
+                <label className="form-check-label" htmlFor="flexCheckDefault">Lembrar-me</label>
               </div>
-              <div className="form-floating">
-                <input type="email" className="form-control" id="floatingInput" placeholder="your-email@gmail.com" />
-                <label htmlFor="floatingInput">E-mail</label>
-              </div>
-              <div className="form-floating">
-                <input type="password" className="form-control" id="floatingPassword" placeholder="Senha" />
-                <label htmlFor="floatingPassword">Senha</label>
-              </div>
-              <div className="esqueceu_lembrar">
-                  <div className="form-check">
-                    <input type="checkbox" className="form-check-input" id="flexCheckDefault" />
-                    <label className="form-check-label" htmlFor="flexCheckDefault">Lembrar-me</label>
-                  </div>
-                  <a href="#" className="esqueceu">Esqueceu sua Senha?</a>
-              </div>
-              <button className="entrar">Entrar</button>
-              <div className="cadastro">
-                  <Link to="/cadastro" className="cadastrar">Ainda não tem uma cadastro? <strong>Cadastre-se</strong></Link>
-              </div>
-            </form>
-          </div>
-        </main>
-    <Footer />
+              <a href="#" className="esqueceu">Esqueceu sua Senha?</a>
+            </div>
+
+            <button className="entrar" type="submit">Entrar</button>
+
+            <div className="cadastro">
+              <Link to="/cadastro" className="cadastrar">
+                Ainda não tem um cadastro? <strong>Cadastre-se</strong>
+              </Link>
+            </div>
+          </form>
+        </div>
+      </main>
+      <Footer />
     </>
   )
 }
